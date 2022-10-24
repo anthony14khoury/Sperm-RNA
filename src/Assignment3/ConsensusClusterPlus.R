b619b19dfeafc39c66e1aec470fffe35e26d0a26
mhayworth <73895101+mhayworth@users.noreply.github.com>
2022-10-24 15:27:41 -0400

    updated consensusclusterplus to manually generate data

--- src/Assignment3/ConsensusClusterPlus.R
+++ src/Assignment3/ConsensusClusterPlus.R
@@ -1,9 +1,8 @@
 data_dir = file.path("UF/data", "SRP053246")
-data_file = file.path(data_dir, "5000VariableGenes.csv")
+data_file = file.path(data_dir, "CleanedData.tsv")
 
-df = read.csv(data_file, header=TRUE, sep=",")
+df = readr::read_tsv(data_file)
 View(df)
-#Data is already the 5000 most variable genes, sorted and prepared using Excel.
 
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
@@ -12,10 +11,44 @@ BiocManager::install("ConsensusClusterPlus")
 
 library(ConsensusClusterPlus)
 
+num_genes = 10
 d = data.matrix(df, rownames.force=NA)
+mads=apply(d,1,mad)
+d=d[rev(order(mads))[1:num_genes],]
 d = sweep(d,1, apply(d,1,median,na.rm=T))
+results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",title=as.character(num_genes),distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
 
-results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
+num_genes = 100
+d = data.matrix(df, rownames.force=NA)
+mads=apply(d,1,mad)
+d=d[rev(order(mads))[1:num_genes],]
+d = sweep(d,1, apply(d,1,median,na.rm=T))
+results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",title=as.character(num_genes),distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
+
+num_genes = 1000
+d = data.matrix(df, rownames.force=NA)
+mads=apply(d,1,mad)
+d=d[rev(order(mads))[1:num_genes],]
+d = sweep(d,1, apply(d,1,median,na.rm=T))
+results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",title=as.character(num_genes),distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
+
+
+num_genes = 5000
+d = data.matrix(df, rownames.force=NA)
+mads=apply(d,1,mad)
+d=d[rev(order(mads))[1:num_genes],]
+d = sweep(d,1, apply(d,1,median,na.rm=T))
+results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",title=as.character(num_genes),distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
+
+
+num_genes = 10000
+d = data.matrix(df, rownames.force=NA)
+mads=apply(d,1,mad)
+d=d[rev(order(mads))[1:num_genes],]
+d = sweep(d,1, apply(d,1,median,na.rm=T))
+results = ConsensusClusterPlus(d,maxK=20,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",title=as.character(num_genes),distance="pearson",seed=1262118388.71279,plot="pdf",writeTable=TRUE)
+
+
 
 results[[2]][["consensusMatrix"]][1:5,1:5]
 results[[2]][["consensusTree"]]
